00d35927412a399471efadfd522f276232917e70
Gabriel Barbarino <gabriel.barbarino13@gmail.com>
2025-09-01 21:40:01 -0300

    aa

--- appElevador/src/server.ts
+++ appElevador/src/server.ts
@@ -19,7 +19,7 @@ const angularApp = new AngularNodeAppEngine();
  * Example:
  * ```ts
  * app.get('/api/{*splat}', (req, res) => {
- *   // Handle API request
+ * // Handle API request
  * });
  * ```
  */
@@ -53,11 +53,7 @@ app.use((req, res, next) => {
  */
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
-
+  app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
