443357ba23e6918d9d5abef82aca7007f59f612b
FuturoDevTeu <159257681+FuturoDevTeu@users.noreply.github.com>
2025-09-01 21:54:08 -0300

    Update ComponenteService.java

--- abeel/src/main/java/br/com/abeel/abeel/service/ComponenteService.java
+++ abeel/src/main/java/br/com/abeel/abeel/service/ComponenteService.java
@@ -67,7 +67,7 @@ public class ComponenteService {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error","Nome inválido (mínimo 3 caracteres alfabéticos)"));
         }
 
-        if (cr.existsByNomeAndIdNot(nome, idComponente)) {
+        if (!cr.existsByNomeAndIdNot(nome, idComponente)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error","Esse componente já existe"));
         }
 
