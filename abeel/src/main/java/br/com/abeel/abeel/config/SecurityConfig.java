509340e24fcd8dcf9de35096ddbab33c325fce54
Gabriel Barbarino <gabriel.barbarino13@gmail.com>
2025-10-09 15:39:47 -0300

    atualizado

--- abeel/src/main/java/br/com/abeel/abeel/config/SecurityConfig.java
+++ abeel/src/main/java/br/com/abeel/abeel/config/SecurityConfig.java
@@ -70,7 +70,7 @@
         @Bean
         public CorsConfigurationSource corsConfigurationSource(){
             CorsConfiguration config = new CorsConfiguration();
-            config.setAllowedOriginPatterns(List.of("https://aaabeel.vercel.app"));
+            config.setAllowedOriginPatterns(List.of("http://localhost:4200"));
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
             config.setAllowedHeaders(List.of("*"));
             config.setAllowCredentials(true);
